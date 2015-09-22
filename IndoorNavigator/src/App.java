2f07e9e1821f33ad45c193f2e5c9e0481f203247
jzho91 <kiss520szyr@hotmail.com>
2015-09-22 21:48:16 +1000

    redirection added in A* path finding

--- IndoorNavigator/src/App.java
+++ IndoorNavigator/src/App.java
@@ -4,7 +4,7 @@ import java.util.PriorityQueue;
 
 public class App {
 	
-	public static Tile getLocationTile(String location, Map map) {
+	public Tile getLocationTile(String location, Map map) {
 		for (int x=0; x<map.getXLength(); x++) {
 			for (int y=0; y<map.getYLength(); y++) {
 				if (map.getTile(x, y).getType().name() == "location") {
@@ -15,68 +15,94 @@ public class App {
 		return null;
 	}
 	
-	public static void expandCurrentTile(Tile current, ArrayList<Tile> open, ArrayList<Tile> close, Map map) {
+	public void redirection(PriorityQueue<Tile> open, ArrayList<Tile> expandedTiles, Tile current) {
+		//counter for expandedTile
+		int i=0;
+		//the loop go through each element in expandedTiles
+		while (i<expandedTiles.size()) {
+			//if the tile in expandedTile is already in open
+			if (open.contains(expandedTiles.get(i))) {
+				//transfer this open to a array list
+				ArrayList<Tile> openArray = new ArrayList<>(open);
+				//if the total cost from its current parent is  greater than its new parent then we redirect it
+				if (openArray.get(openArray.indexOf(expandedTiles.get(i))).getfCost() > expandedTiles.get(i).getfCost()) {
+					//here is where the redirection occurred
+					openArray.get(openArray.indexOf(expandedTiles.get(i))).setParent(current);
+				}
+				//no matter redirection occurred or not, we delete the tile from expandedTiles
+				expandedTiles.remove(i);
+				//here we reconstruct open back to a priority queue
+				open.clear();
+				for (int j=0; i<openArray.size(); j++) {
+					open.add(openArray.get(j));
+				}
+			}
+			i++;
+		}
+	}
+	
+	public void expandCurrentTile(Tile current, PriorityQueue<Tile> open, ArrayList<Tile> close, Map map) {
 		ArrayList<Tile> expandedTiles = new ArrayList<>();
 		//add all connected tiles (with current) to expandedTiles
 		if (current.getU() != null && !close.contains(current.getU())) {
 			current.getU().setgCost(current.getgCost()+1);
-			current.getU().setfCost(current.getU().getgCost()+current.getU().gethCost());
+			current.getU().setfCost(current.getU().getgCost());
 			expandedTiles.add(current.getU());
 		}
 		if (current.getD() != null && !close.contains(current.getD())) {
 			current.getD().setgCost(current.getgCost()+1);
-			current.getD().setfCost(current.getD().getgCost()+current.getD().gethCost());
+			current.getD().setfCost(current.getD().getgCost());
 			expandedTiles.add(current.getD());
 		}
 		if (current.getL() != null && !close.contains(current.getL())) {
 			current.getL().setgCost(current.getgCost()+1);
-			current.getL().setfCost(current.getL().getgCost()+current.getL().gethCost());
+			current.getL().setfCost(current.getL().getgCost());
 			expandedTiles.add(current.getL());
 		}
 		if (current.getR() != null && !close.contains(current.getR())) {
 			current.getR().setgCost(current.getgCost()+1);
-			current.getR().setfCost(current.getR().getgCost()+current.getR().gethCost());
+			current.getR().setfCost(current.getR().getgCost());
 			expandedTiles.add(current.getR());
 		}
 		if (current.getLU() != null && !close.contains(current.getLU())) {
 			current.getLU().setgCost(current.getgCost()+1);
-			current.getLU().setfCost(current.getLU().getgCost()+current.getLU().gethCost());
+			current.getLU().setfCost(current.getLU().getgCost());
 			expandedTiles.add(current.getLU());
 		}
 		if (current.getUR() != null && !close.contains(current.getUR())) {
 			current.getUR().setgCost(current.getgCost()+1);
-			current.getUR().setfCost(current.getUR().getgCost()+current.getUR().gethCost());
+			current.getUR().setfCost(current.getUR().getgCost());
 			expandedTiles.add(current.getUR());
 		}
 		if (current.getLD() != null && !close.contains(current.getLD())) {
 			current.getLD().setgCost(current.getgCost()+1);
-			current.getLD().setfCost(current.getLD().getgCost()+current.getLD().gethCost());
+			current.getLD().setfCost(current.getLD().getgCost());
 			expandedTiles.add(current.getLD());
 		}
 		if (current.getRD() != null && !close.contains(current.getRD())) {
 			current.getRD().setgCost(current.getgCost()+1);
-			current.getRD().setfCost(current.getRD().getgCost()+current.getRD().gethCost());
+			current.getRD().setfCost(current.getRD().getgCost());
 			expandedTiles.add(current.getRD());
 		}
-		Comparator<Tile> testComparator = new Comparator<Tile>() {
-			
+		//redirection
+		redirection(open, expandedTiles, current);
+		
+	}
+	
+	public ArrayList<String> aStarPathFinding(Tile startTile, Tile endTile, Map map) {
+		//initialize the path, path will contains the finally route
+		ArrayList<String> path = new ArrayList<>();
+		//initialize the current, current is the node we choose to expand in each iteration
+		Tile current = startTile;
+		//initialize open and close lists
+		Comparator<Tile> theComparator = new Comparator<Tile>() {
 			@Override
 			public int compare(Tile o1, Tile o2) {
 				// TODO Auto-generated method stub
 				return o2.getfCost() - o1.getfCost();
 			}
 		};
-		PriorityQueue<Tile> test = new PriorityQueue<>(testComparator);
-		
-	}
-	
-	public static ArrayList<String> aStarPathFinding(Tile startTile, Tile endTile, Map map) {
-		//initialize the path, path will contains the finally route
-		ArrayList<String> path = new ArrayList<>();
-		//initialize the current, current is the node we choose to expand in each iteration
-		Tile current = startTile;
-		//initialize open and close lists
-		ArrayList<Tile> open = new ArrayList<>();
+		PriorityQueue<Tile> open = new PriorityQueue<>(theComparator);
 		open.add(current);
 		ArrayList<Tile> close = new ArrayList<>();
 		for (;;) {
@@ -85,7 +111,7 @@ public class App {
 				return null;
 			}
 			//choose the tile with the lowest f
-			current = open.get(0);
+			current = open.poll();
 			//check if we reached the goal
 			if (current.getType().name() == endTile.getType().name()) {
 				return path;
@@ -94,7 +120,7 @@ public class App {
 		}
 	}
 	
-	public static void findOutRoute(String initialLocation, String destination, String building, String level) {
+	public void findOutRoute(String initialLocation, String destination, String building, String level) {
 		Map map = null;
 		if (building == "Campus centre") {
 			if (level == "Ground") {
